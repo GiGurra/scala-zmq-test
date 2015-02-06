950985311e2844bc5964a996c5565c67bc9ebc04
Johan Kjölhede <kjolhede@telia.com>
2015-02-06 20:17:32 +0100

    more experiments

--- src/main/scala/se/gigurra/scala_zmq_test/Main.scala
+++ src/main/scala/se/gigurra/scala_zmq_test/Main.scala
@@ -1,8 +1,5 @@
 package se.gigurra.scala_zmq_test
 
-import java.nio.charset.Charset
-
-import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -10,7 +7,6 @@ import scala.concurrent.duration.DurationInt
 import scala.language.postfixOps
 
 import org.zeromq.ZContext
-import org.zeromq.ZMQ.Socket
 
 import zmq.ZMQ
 
@@ -18,58 +14,42 @@ object Main {
 
   def main(args: Array[String]) {
 
-    val port = 12345
-    val addr = s"tcp://127.0.0.1:$port"
+    val port = 8080
+    val ip = "192.168.1.211"
 
     val client = Future {
 
       val ctx = new ZContext
       val socket = ctx.createSocket(ZMQ.ZMQ_REQ)
-      socket.connect(addr)
+      socket.connect(s"tcp://$ip:$port")
       socket.send("Hello")
 
-      val reply = socket.recvStr()
-      println(s"Client received: $reply")
+      println(s"Client received: ${socket.recvStr()}")
 
       ctx.close()
     }
 
+    /*
     val server = Future {
 
       val ctx = new ZContext
       val socket = ctx.createSocket(ZMQ.ZMQ_ROUTER)
-      socket.bind(addr)
+      socket.bind(s"tcp://0.0.0.0:$port")
 
       val pieces = recvAll(socket)
 
-      for (piece <- pieces) {
+      for (piece <- pieces)
         println(s"Server received: ${new String(piece, Charset.defaultCharset)}")
-      }
 
-      sendAll(socket, pieces)
+      Util.sendAll(socket, pieces)
 
       ctx.close()
     }
 
-    Await.result(server, 10 seconds)
-    Await.result(client, 10 seconds)
-  }
+    Await.result(server, 2 seconds)
+    */
 
-  def recvAll(socket: Socket): Seq[Array[Byte]] = {
-    val pieces = new ArrayBuffer[Array[Byte]]
-    pieces += socket.recv()
-    while (socket.hasReceiveMore())
-      pieces += socket.recv()
-    pieces
-  }
-
-  def sendAll(socket: Socket, pieces: Seq[Array[Byte]]) {
-    for (piece <- pieces) {
-      if (piece != pieces.last)
-        socket.sendMore(piece)
-      else
-        socket.send(piece)
-    }
+    Await.result(client, 2 seconds)
   }
 
 }
